4daedb6f23bf5eba131f97f89b7e892f70e2a879
Ankush Kailas Dhavale <dhavaleankush7@gmail.com>
2024-01-25 21:16:53 +0530

    Learn Array

---
+++ array/basicArray.java
@@ -0,0 +1,10 @@
+package array;
+
+public class basicArray {
+    public static void main(String[] args) {
+        int num[] = new int[10];
+        for(int i=0; i<num.length; i++){
+            System.out.println(num[i]);
+        }
+    }    
+}
